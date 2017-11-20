95afea4d2de1a61f995c5499947fca355e042cf0
isabelacastedo <isabelacastedo@hotmail.com>
2017-11-20 00:59:10 -0300

    EH TETRAAAAAA

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,92 +1,82 @@
-<<<<<<< HEAD
-$(document).ready(function () {
+document.addEventListener("deviceready", function(){
 
 
 	//Adicionando Copos na Div
 	var copos = 0;
 	var contador = 0;
+
+	//IF QUANDO O APP INCIAR ANTES DE CLICAR O BOTAO
+	if (copos == 0) {
+		$("#texto-sangue").html("Seu volume sanguíneo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
+		$("#texto-cerebro").html("Seu cérebro está mais cansado e você pode ter dores de cabeça.");
+		$("#texto-rim").html("Seu rim está recebendo menos oxigênio e causando lesões em suas células. Há mais chances de desenvolver pedras nos rins e infecções urinárias.");
+		$("#texto-estomago").html("Há dificuldade em digerir os alimentos e eliminar as toxinas.");
+	};
+
+
 	$("#btnAddCopo").click(function () {
 		$(".cups").append('<img src="img/agua.png">');
 
-=======
-	$(document).ready(function() {
-  
-
-	//Adicionando Copos na Div
-	var copos = 0;
-	var contador=0;
-	$("#btnAddCopo").click(function() {
-		$(".cups").append( '<img src="img/agua.png">');
-			
->>>>>>> master
 		contador++;
 		console.log(contador);
 		copos = contador;
 
 
 		//Desidratado
-<<<<<<< HEAD
 		if (copos >= 0 && copos <= 2) {
 			$("#user-foto").attr("src", "img/desidratado.png");
 			$("#status").html("Você está desidratado, tome alguns copos de água para seu corpo funcionar corretamente!");
-			$("#texto-sangue").html("Seu volume sangúineo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
 		};
 
 
 		//Semidesidratado
 		if (copos > 2 && copos <= 4) {
 			$("#user-foto").attr("src", "img/semidesidratado.png");
-=======
-		if(copos>=0 && copos<=2){
-				$( "#user-foto").attr( "src", "img/desidratado.png" );
-				$("#status").html("Você está desidratado, tome alguns copos de água para seu corpo funcionar corretamente!");
-			};
-
-
-		//Semidesidratado
-		if(copos>2 && copos<=4){
-			$( "#user-foto").attr( "src", "img/semidesidratado.png" );
->>>>>>> master
 			$("#status").html("Você está quase semidesidratado, tome alguns copos de água para seu corpo funcionar corretamente!");
 		};
 
 		//Semihidratado
-<<<<<<< HEAD
 		if (copos > 4 && copos <= 7) {
 			$("#user-foto").attr("src", "img/semihidratado.png");
-=======
-		if(copos>4 && copos<=7){
-			$( "#user-foto").attr( "src", "img/semihidratado.png" );
->>>>>>> master
 			$("#status").html("Você está quase hidratado, tome alguns copos de água para seu corpo funcionar corretamente!");
 		};
 
 		//Hidratado
-<<<<<<< HEAD
 		if (copos > 7) {
 			$("#user-foto").attr("src", "img/hidratado.png");
-=======
-		if(copos>7){
-			$( "#user-foto").attr( "src", "img/hidratado.png" );
->>>>>>> master
 			$("#status").html("Você está hidratado!, isso que é vida saudável!");
+		};
+
+
+		//IFS DA TELA TRES!! :)
+		//Desidratado
+		if (copos >= 0 && copos <= 3) {
+			$("#texto-sangue").html("Seu volume sanguíneo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
+			$("#texto-cerebro").html("Seu cérebro está mais cansado e você pode ter dores de cabeça.");
+			$("#texto-rim").html("Seu rim está recebendo menos oxigênio e causando lesões em suas células. Há mais chances de desenvolver pedras nos rins e infecções urinárias.");
+			$("#texto-estomago").html("Há dificuldade em digerir os alimentos e eliminar as toxinas.");
+		};
+
+		//Semidesidratado
+		if (copos >= 4 && copos <= 7) {
+			$("#texto-sangue").html("Seu volume sanguíneo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
+			$("#texto-cerebro").html("Seu volume sanguíneo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
+			$("#texto-rim").html("Seu volume sanguíneo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
+			$("#texto-estomago").html("Seu volume sanguíneo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
+		};
+
+		//Hidratado
+		if (copos > 7) {
+			$("#texto-sangue").html("Seu volume sanguíneo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
+			$("#texto-cerebro").html("Seu volume sanguíneo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
+			$("#texto-rim").html("Seu volume sanguíneo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
+			$("#texto-estomago").html("Seu volume sanguíneo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
 		};
 
 	});
 
 	console.log(copos);
 
-<<<<<<< HEAD
 	//Para user Status
 
 });
-=======
-
-	
-
-
-
-	//Para user Status
-
-	});
->>>>>>> master
