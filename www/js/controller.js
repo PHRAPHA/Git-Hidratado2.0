573ce9e816b55d0fe3a91aed32da0dc723b3261c
isabelacastedo <isabelacastedo@hotmail.com>
2017-11-20 16:39:32 -0300

    eu sou uma mula

--- www/js/controller.js
+++ www/js/controller.js
@@ -3,31 +3,18 @@ document.addEventListener("deviceready", function(){
 
 	//Adicionando Copos na Div
 	var copos = 0;
-	var contador = 0;
 
-	//IF QUANDO O APP INCIAR ANTES DE CLICAR O BOTAO
-	if (copos == 0) {
-		$("#texto-sangue").html("Seu volume sanguíneo está reduzido e a pressão sanguínea pode cair. A falta de água também prejudica o transporte de nutrientes para outros órgãos.");
-		$("#texto-cerebro").html("Seu cérebro está mais cansado e você pode ter dores de cabeça.");
-		$("#texto-rim").html("Seu rim está recebendo menos oxigênio e causando lesões em suas células. Há mais chances de desenvolver pedras nos rins e infecções urinárias.");
-		$("#texto-estomago").html("Há dificuldade em digerir os alimentos e eliminar as toxinas.");
-	};
-
-
+	//BOTAO QUE O USUARIO CLICA PRA ADICIONAR COPO
 	$("#btnAddCopo").click(function () {
 		$(".cups").append('<img src="img/agua.png">');
 
-		contador++;
-		console.log(contador);
-		copos = contador;
-
+		copos++;
 
 		//Desidratado
 		if (copos >= 0 && copos <= 2) {
 			$("#user-foto").attr("src", "img/desidratado.png");
 			$("#status").html("Você está desidratado, tome alguns copos de água para seu corpo funcionar corretamente!");
 		};
-
 
 		//Semidesidratado
 		if (copos > 2 && copos <= 4) {
@@ -76,7 +63,4 @@ document.addEventListener("deviceready", function(){
 	});
 
 	console.log(copos);
-
-	//Para user Status
-
 });
